b543c7f0a1d36e66cdf3e01f49f3c5dddd2d27b6
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-18 14:10:22 +0000

    modification of printf command with inversion of the command

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,6 +7,10 @@
 
 int main(void)
 {
-	scanf(const char 0);
+	printf("size of a char: %d byte(s) \n",Size of (char)); 
+	printf("size of a int: %d byte(s) \n",Size of (int));
+	printf("size of a long int: %d byte(s) \n",Size of (long int));
+	printf("size of a long long int: %d byte(s) \n",Size of (long long int));
+	printf("size of a float: %d byte(s) \n",Size of (float));
 	return (0);
 }
