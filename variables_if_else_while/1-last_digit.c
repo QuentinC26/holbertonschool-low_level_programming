9c926a9190752645cdcecc48c37d09bd1de2f752
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-19 22:05:45 +0000

    add a multiple if and variable z with a division

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,11 +11,22 @@
 int main(void)
 {
 	int n;
+	int z;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%s Last digit of"\n, n "is" \n, "and is greater than 5")
-	printf("%s Last digit of"\n, n "is" \n, "and is 0")
-	printf("%s Last digit of"\n, n "is" \n, "and is less than 6 and not 0")
+	z = n % 10;
+	if (z > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, z);
+	}
+	if (z == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, z);
+	}
+	if (z < 6 && z != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, z);
+	}
 	return (0);
 }
