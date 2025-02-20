bd43e0679afd435a7020dc620856b8492fc1250d
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-20 21:53:38 +0000

    add a code with putchar and for

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+	{
+	char v;
+
+	for (v = 'a'; v <= 'z'; v++)
+if ?
+tr ?
+while ?
+	putchar(v);
+	putchar('\n');
+	return (0);
+	}
