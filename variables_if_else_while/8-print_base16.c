5c5f63088e2bcaa13ee703a24e54156b92df15f5
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-21 10:59:19 +0000

    im add a letter f in the second for command et im a remove e in the second for command

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@
 
 	for (base_16 = '0'; base_16 <= '9'; base_16++)
 		putchar(base_16);
-	for (base_16 = 'a'; base_16 <= 'e'; base_16++)
+	for (base_16 = 'a'; base_16 <= 'f'; base_16++)
 		putchar(base_16);
 	putchar('\n');
 	return (0);
