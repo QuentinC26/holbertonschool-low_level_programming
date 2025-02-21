a83b14a25b69ecafcb19ee7cb7ad33f3e8a92596
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-21 10:13:40 +0000

    i change ++ with -- and i change <= with >=

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -11,8 +11,8 @@
 	{
 	char reverse_alphabet;
 
-	for (reverse_alphabet = 'z'; reverse_alphabet <= 'a'; reverse_alphabet++)
-		putchar(reverse_alphabet++);
+	for (reverse_alphabet = 'z'; reverse_alphabet >= 'a'; reverse_alphabet--)
+		putchar(reverse_alphabet);
 	putchar('\n');
 	return (0);
 	}
