5bc4850b870a5ffa295ae7a4333cb20d8fe28721
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-22 23:52:34 +0000

    add a code with two _putchar command

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*main - Entry Point
+*
+*Return: 0 (Success)
+*/
+
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar (alphabet);
+_putchar ('\n');
+return (0);
+}
