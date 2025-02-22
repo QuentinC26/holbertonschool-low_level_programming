2d7ea3f2a49abacfff91c32d1268f84540919498
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-22 23:21:07 +0000

    i remove three lines and i add int _putchar

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,34 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
-void sayHello();
-int add(int a, int b);
-double area(double radius);
-int length(char *x);
+int _putchar(char c);
 
-#endif
-
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-
-#define PI 3.142
-
-void sayHello(){
-   printf("Hello World\n");
-}
-
-int add(int a, int b){
-   int result;
-   result = a+b;
-   return result;
-}
-
-double area(double radius){
-   double areaofcircle = PI*pow(radius, 2);
-   return areaofcircle;
-}
-
-int length(char *x){
-   return strlen(x);
-}
+#endif /* SAMPLE_MAIN_H */
