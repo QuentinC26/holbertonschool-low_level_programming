c06e64da27edf966c8e4b421101ee6b77ec81f79
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-23 19:08:22 +0000

    add a int _islower(int c); prototype

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);  /** Function for _putchar */
 void print_alphabet(void);  /** Declaration of function print_alphabet */
-void print_alphabet_x10(void);	/**Declaration of funtionc prin_alphabet_x10*/
+void print_alphabet_x10(void);	/**Declaration of funtion prin_alphabet_x10*/
+int _islower(int c); /**Declaration of function for check a letter lowercase*/
 
 #endif /* MAIN_H */
