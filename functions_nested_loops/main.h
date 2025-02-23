88ae36b5271b49109154b8851eaf45d137f0b81d
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-23 14:29:26 +0000

    remove a spaces

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,9 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);  /** Function for _putchar */
-
 void print_alphabet(void);  /** Declaration of function print_alphabet */
-
 void print_alphabet_x10(void);	/**Declaration of funtionc prin_alphabet_x10*/
 
 #endif /* MAIN_H */
