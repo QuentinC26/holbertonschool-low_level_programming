3db988e260cd77d8c36af6fca978ad0fc17dac69
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-23 19:19:34 +0000

    add a two if command and one _putchar command

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - int _islower(int c) is used for checking a lowercase letters
+ *
+ * Return: 0 (Sucess)
+ *
+ */
+
+void print(void)
+
+char = lowercase;
+
+if (lowercase = 'a'; lowercase <= 'z' return 0; lowercase++)
+if (lowercase = 'c'; loercase = return 1; lowercase++)
+_putchar (lowercase);
