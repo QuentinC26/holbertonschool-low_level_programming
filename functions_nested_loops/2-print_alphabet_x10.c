c0755af16612315605ba6b0c2c853c280f94a5cf
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-23 13:39:05 +0000

    add a code with 2 _putchar and 2 for

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_alphabet - Entry Point
+*
+*Return: 0 (Success)
+*/
+
+void print(void)
+{
+char tenlinesofalphabet;
+
+for (tenlinesofalphabet = 'a'; tenlinesofalphabet <= 'z'; tenlinesofalphabet++)
+{
+_putchar (tenlinesofalphabet);
+}
+
+int tenlinesofalphabet{10};
+
+for (tenlinsofalphabet < 10; tenlinesofalphabet++)
+{
+_putchar (tenlinesofalphabet);
+_putchar ('\n');
+}
