68a5916577199c39f8803fdcd0d1b4947097f532
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-23 19:52:14 +0000

    modify a description of prototype comment and i modify a if and else command

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _islower(int c) - int _islower(int c) is used for checking a lowercase letters
+ * int _islower(int c) - int _islower(int c) is for check a lowercase letters
  *
  * Return: 0 (Sucess)
  *
@@ -12,11 +12,11 @@ int _islower(int c)
 int c;
 {
 {
-if (c = 'a'; c <= 'z'; c++)
+if (c = 'a', c <= 'z', c++)
 return (1);
 }
 {
 else
 return (0);
-}  
+}
 }
