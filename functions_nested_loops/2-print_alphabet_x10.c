abe5746bb0b242cad7286d8f76a856fd7953565a
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-23 16:18:55 +0000

    returning in my code base with while and for command

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,12 @@
 
 void print_alphabet_x10(void)
 {
-int i{0};
+char tenlines;
 
-while (i < 10)
+while (tenlines < 10)
 {
-std::count << sentence << "\n";
-
-i++;
+for (tenlines = 'a'; tenlines <= 'z'; tenlines++)
+_putchar (tenlines);
 }
 _putchar ('\n');
 }
