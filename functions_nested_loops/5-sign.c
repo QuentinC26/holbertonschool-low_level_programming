02f2e51cc27030c09f8bd89c7882464e11d9da1a
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-24 09:18:05 +0000

    add a 3 if command in the block of the code

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _print_sign - Check the sign of the number.
+ * @n: - 'n' is a variable number
+ *
+ * Return: 1 (Error)
+ *
+ */
+
+int _print_sign(int n)
+
+{
+        if (n > 0)
+        {
+        return (1);
+        }
+        if (n == 0)
+        {
+        return (0);
+        }
+	if (n < 0)
+	{
+	return (-1);
+	}
+}
