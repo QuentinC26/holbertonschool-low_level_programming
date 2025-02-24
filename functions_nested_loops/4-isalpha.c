9ef4a71c80409b2ddb295999fcacb5614bc36d1b
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-24 07:51:33 +0000

    remove & and add | in the if command

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * int _isalpha(int c) - int _isaplha(int c) is for check a any letters
- * _isalpha(int c) - Check the any letters.
+ * _isalpha - Check the any letters.
  * @c: - 'c' is a variable
  *
- * Return: 0 (Sucess)
+ * Return: 1 (Error)
  *
  */
 
 int _isalpha(int c)
 
 {
-if (c <= 'a' && c >= 'z')
-else if (c <= 'A' && c >= 'Z')
-return (1);
+	if (c >= 'a' || c <= 'z')
+	else if (c >= 'A' || c <= 'Z')
+	return (1);
 }
 {
-else
-return (0);
+	else
+	return (0);
 }
