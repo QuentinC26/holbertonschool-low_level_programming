f959dcd093a4d88f96871b695db1115fbb4bb904
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-24 09:07:50 +0000

    add int print_sign(int n);

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);  /** Function for _putchar */
 void print_alphabet(void);  /** Declaration of function print_alphabet */
 void print_alphabet_x10(void);	/**Declaration of funtion prin_alphabet_x10*/
 int _islower(int c); /**Declaration of function for check a letter lowercase*/
-int _isalpha(int c); /**Declaration of foncton for alphabetic letters*/
+int _isalpha(int c); /**Declaration of function for alphabetic letters*/
+int print_sign(int n); /**Declaration of function for number sign*/
 
 #endif /* MAIN_H */
