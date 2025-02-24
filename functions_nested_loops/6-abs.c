0a8e446cd32c17a6cb7e252b4f054d3ba1b0f0e7
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-24 12:25:35 +0000

    add if and else command

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs - Declaration of function for compute the absolute value
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int abs(int)
+
+int number;
+
+{
+if (number >= 0; abs;)
+{
+else (number <= 0; number =!; abs;)
+}
+return (0);
+}
