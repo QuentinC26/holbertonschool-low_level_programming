9fed3c3baecaef883fcbb1d0f66664d86c55223a
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-24 15:11:56 +0000

    add a base code

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Declaration of last digit of number
+ * @???: ????
+ * Return: 0 (Success)
+ */
+
+int print_last_digit(int);
+
+{
+
+
+return (0);
+}
