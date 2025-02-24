ee0b2761dbbe36a8958b481ebb696201c8017a4a
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-24 00:52:00 +0000

    add a if and else command in the code

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - int _islower(int c) is for check a lowercase letters
+  * _islower(int c) - Check the lowercase letters.
+ * @c: - 'c' is a variable
+ *
+ * Return: 0 (Sucess)
+ *
+ */
+
+int _isalpha(int c)
+
+{
+if (c == char)
+return (1);
+else
+return (0);
+}
