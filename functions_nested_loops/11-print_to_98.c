4336ba8036613cce8abb96868372c06158fdb1e0
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-25 22:10:34 +0000

    add if, else if et and else condition for the function

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Function that prints all natural numbers
- *
+ * @n: Variable for the start of the print
  * Return: 0 (Success)
  */
 
 void print_to_98(int n)
 
 {
-
+	for (n = 0; n <= 98; n++)
+	{	
+		if (n == 0)
+		{
+			_putchar('0');
+		}
+		else if (n <= 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(n + '0');
+		}
+		else
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+	}
+	_putchar('\n');
 }
