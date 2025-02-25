907d319cdfa9c55a8f655e22e32e07439176b868
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-25 13:48:41 +0000

    add int column

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,5 +8,24 @@
 void times_table(void)
 
 {
-	?
+	int table;
+	int column;
+
+	for (table = 0; table <= 9, table ++)
+	{
+		for (column = 0; column <= 9, column ++)
+		
+			{	
+				_putchar(table * column + '0');
+				_putchar(table * column + '1');
+				_putchar(table * column + '2');
+				_putchar(table * column + '3');
+				_putchar(table * column + '4');
+				_putchar(table * column + '5');
+				_putchar(table * column + '6');
+				_putchar(table * column + '7');
+				_putchar(table * column + '8');
+				_putchar(table * column + '9');
+			}
+	}
 }
