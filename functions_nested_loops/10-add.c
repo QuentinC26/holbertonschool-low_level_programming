9ccb1e12dfef6a5318a10852058115545a91e725
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-25 21:11:52 +0000

    add a addition in the code

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
-/** add.c - Functions for add two integers
+/**
+ * add - Functions for add two integers
+ * @a: First value for addition
+ * @b: Second value for addition
  * Return: 0 (Success)
  */
 
-int add(int, int);
+int add(int a, int b)
 
 {
-	?
+	int result;
+
+	result = a + b;
+	return (result);
 }
