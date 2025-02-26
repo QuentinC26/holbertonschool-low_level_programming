33ba14c5f9d9c8db48f87b636d48844da98d92b9
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-26 12:23:15 +0000

    add a prototype void swap_int(int *a, int *b);

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H_
 
 void reset_to_98(int *n); /* command for update the value of the pointers*/
+void swap_int(int *a, int *b); /*command for swap the value of two integer number*/
 
 #endif /* MAIN_H_ */
