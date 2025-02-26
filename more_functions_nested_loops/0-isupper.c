6de7ee8ff88379634926c1b2c0b807858b1c41a9
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-26 07:44:22 +0000

    remove a for and modify a if condition

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -9,16 +9,9 @@
 int _isupper(int c)
 
 {
-	for (c = 'A'; c <= 'Z'; c++)
+	if (c >= 'A' && c <= 'Z')
 	{
-		if (c == 'A')
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
 return (0);
 }
