556a4bcd399fe0655db97ed7afa25ef7ecfc4371
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-26 12:29:43 +0000

    add a base code for this exercise

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Function for swap the value of two integers
+ * @,: Description a mettre
+ * Return: 1 (Error)
+ */
+
+void swap_int(int *a, int *b)
+
+{
+
+}
