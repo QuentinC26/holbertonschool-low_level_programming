494aa7ae5677d2c26fa78cb17a6dcc5f2e392b68
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-27 23:15:52 +0000

    add void _puts(char *str); prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 void reset_to_98(int *n); /* command for update the value of the pointers*/
 void swap_int(int *a, int *b); /*command for swap the value of two integer number*/
 int _strlen(char *s); /* command for return the length of the string*/
+void _puts(char *str); /* function for that prints a string*/
 
 #endif /* MAIN_H_ */
