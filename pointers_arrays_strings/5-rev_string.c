08fe009c03c3d1d8b25cbf36fdd0c5fd4a2e9f9f
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-28 13:48:17 +0000

    add for and while loop for prints a string and for reverse the string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,13 +2,28 @@
 
 /**
  * rev_string - Functions for reverse the string
- * @?: ???
+ * @s: Variable with pointer.
  * Return: 1 (Error)
  */
 
 void rev_string(char *s)
 {
+	int lenght = 0;
+	char temporaire;
+	int start = 0;
+	int end = 0;
 
-???
+	for (; *s != '\0'; s++)
+	{
+		lenght++;
+	}
+	while (start < end)
+	{
+		temporaire = s[start];
+		s[start] = s[end];
+		s[end] = temporaire;
 
+		start++;
+		end--;
+	}
 }
