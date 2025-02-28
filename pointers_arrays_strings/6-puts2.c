a2638bd3bee94f269171fefde65d209e834d2aec
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-28 15:53:38 +0000

    add a for and if command

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- * void puts2 - Function for prints every other character of a string.
- * @?: ???
+ * puts2 - Function for prints every other character of a string.
+ * @str: Variable with pointer
  * Return: 1 (error):
  */
 
 void puts2(char *str)
 {
+	int n;
 
-	???
-
+	for (n = 0; n != '\0'; n++)
+	{
+		if (n % 2 == 0)
+		{
+			_putchar (str[n]);
+		}
+	}
 }
