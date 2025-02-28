a9cd24b3c25abc1b402081dc95569da7ec76c720
QuentinC26 <q.zuzlewski@gmail.com>
2025-02-28 10:12:10 +0000

    add _putchar prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+int _putchar(char c); /* command putchar*/
 void reset_to_98(int *n); /* command for update the value of the pointers*/
 void swap_int(int *a, int *b); /*command for swap the value of two integer number*/
 int _strlen(char *s); /* command for return the length of the string*/
