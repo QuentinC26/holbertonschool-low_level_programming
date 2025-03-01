1bf8db887583803782d019e4afc15ae4f78f2b70
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 15:39:51 +0000

    add a if condition for prints the _ or not

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,19 @@
 
 /**
  * print_line - function that draws a straight line in the terminal.
- * @?: ???
+ * @n: Variable for draws a straight line
  * Return: 1 (Error)
  */
 
 void print_line(int n)
 {
-
-
-
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(_);
+		_putchar('\n');
+	}
 }
