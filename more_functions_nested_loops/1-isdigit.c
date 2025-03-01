1e1f512674d53221e92d68ab0247c551f131e646
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 10:13:17 +0000

    add a if condition

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,15 @@
 
 /**
  * _isdigit - Function for check the digit
- * @?: ???
+ * @c: Variable for check the digit
  * Return: 0 (Success)
  */
 
 int _isdigit(int c)
 {
-
-
+	if (c == 0 && c >= 9)
+	{
+		return (1);
+	}
+return (0);
 }
