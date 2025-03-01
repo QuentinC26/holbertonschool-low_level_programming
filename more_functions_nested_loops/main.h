37fdf3a797fe2badc4f02c15430cd42b025b8500
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 15:20:46 +0000

    add void print_line(int n); prototype

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int mul(int a, int b); /**Function for  multiplies two integers*/
 void print_numbers(void); /**Function for prints the number from 0 at 9*/
 void print_most_numbers(void); /**Function for prints the number without 2 and 4*/
 void more_numbers(void); /**Function for prints 10 times number from 0 at 14*/
+void print_line(int n); /** function that draws a straight line*/
 
 #endif /* MAIN_H */
