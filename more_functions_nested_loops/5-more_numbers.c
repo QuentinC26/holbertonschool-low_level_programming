267a202f9139aacd625eb3ee8cc576942ec125d4
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 16:17:10 +0000

    add a a modulus and divison in the second loop for

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,15 @@
 void more_numbers(void)
 {
 	int times;
-	int digit;
 	int number;
 
 	for (times = 0; times < 10; times++)
 	{
-		for (digit = '0'; digit <= '9'; digit++)
+		for (number = 0; number <= 14; number++)
 		{
-			for (number = 10; number <= 14; number++)
-			{
-				_putchar(number);
-			}
-			_putchar(digit);
+			_putchar(number / 10 + '0');
+			_putchar(number % 10 + '0');
 		}
+	_putchar('\n');
 	}
-	_putchar (times);
 }
