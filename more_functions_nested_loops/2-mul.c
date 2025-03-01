e9e2465c2aef3bc02282c2bfeaea9d4cb52457f7
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 10:59:57 +0000

    change the return and add int result before the a * b

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,12 +2,15 @@
 
 /**
  * mul - Function for multiplies two integers
- * @?: ???
+ * @a: First integer for multiplies
+ * @b: Second interger for multiplies
  * Return: 0 (Sucess)
  */
 
 int mul(int a, int b)
 {
+	int result;
 
-
+	result = a * b;
+	return (result);
 }
