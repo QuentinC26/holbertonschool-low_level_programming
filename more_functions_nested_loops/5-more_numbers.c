a112b3a1c7794e39fe5bd1ee97feeb885444233d
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 14:42:06 +0000

    add a 3 for loop in the code

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
-/** more_numbers - Function that prints 10 times the numbers, from 0 to 14
+/**
+ * more_numbers - Function that prints 10 times the numbers, from 0 to 14
  * @?: ???
  * Return: 1 (Error)
  */
 
 void more_numbers(void)
 {
+	int times;
+	int digit;
+	int number;
 
-
-
+	for (times = 0; times < 10; times++)
+	{
+		for (digit = '0'; digit <= '9'; digit++)
+		{
+			for (number = 10; number <= 14; number++)
+			{
+				_putchar(number);
+			}
+			_putchar(digit);
+		}
+	}
+	_putchar (times);
 }
