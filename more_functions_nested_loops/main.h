361580f46de0a74bb817854267904830bf88394a
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 09:54:50 +0000

    add a two prototypes in the code

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c); /**Function for add a _putchar command in the code*/
 int _isupper(int c); /** Function for check upercase*/
+int _isdigit(int c); /** Function for check digit*/
 
 #endif /* MAIN_H */
