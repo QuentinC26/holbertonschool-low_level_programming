2b40c917c65056b95ae766249501d380b9f46de5
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 10:40:49 +0000

    add int mul(int a, int b);

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c); /**Function for add a _putchar command in the code*/
 int _isupper(int c); /** Function for check upercase*/
 int _isdigit(int c); /** Function for check digit*/
+int mul(int a, int b); /** Function for  multiplies two integers*/
 
 #endif /* MAIN_H */
