0dfdaf4d3580fabde5b262444c212da0d7631075
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 11:04:33 +0000

    add void print_numbers(void); prototype

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,8 +2,9 @@
 #define MAIN_H
 
 int _putchar(char c); /**Function for add a _putchar command in the code*/
-int _isupper(int c); /** Function for check upercase*/
-int _isdigit(int c); /** Function for check digit*/
-int mul(int a, int b); /** Function for  multiplies two integers*/
+int _isupper(int c); /**Function for check upercase*/
+int _isdigit(int c); /**Function for check digit*/
+int mul(int a, int b); /**Function for  multiplies two integers*/
+void print_numbers(void); /**Function for prints the number of 0 at 9*/
 
 #endif /* MAIN_H */
