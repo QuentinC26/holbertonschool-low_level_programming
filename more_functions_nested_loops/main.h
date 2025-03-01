679bce516b59ab704c5ed9943be1239dc87b8810
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-01 13:43:31 +0000

    add void more_numbers(void); prototype

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,7 +5,8 @@ int _putchar(char c); /**Function for add a _putchar command in the code*/
 int _isupper(int c); /**Function for check upercase*/
 int _isdigit(int c); /**Function for check digit*/
 int mul(int a, int b); /**Function for  multiplies two integers*/
-void print_numbers(void); /**Function for prints the number of 0 at 9*/
-void print_most_numbers(void); /**Function for prints the number withou 2 and 4*/
+void print_numbers(void); /**Function for prints the number from 0 at 9*/
+void print_most_numbers(void); /**Function for prints the number without 2 and 4*/
+void more_numbers(void); /**Function for prints 10 times number from 0 at 14*/
 
 #endif /* MAIN_H */
