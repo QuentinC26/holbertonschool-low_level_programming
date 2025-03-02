0440bcd5951cdab28d38b9a4e26265b4edc82785
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-02 09:37:10 +0000

    add a if and else condition with for loop in the else

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,12 +2,24 @@
 
 /**
  * print_diagonal - Function that draws a diagonal line on the terminal
- * @?: ??
+ * @n: Variable for that draws a diagonal line
  * Return: 1 (Error)
  */
 
 void print_diagonal(int n)
 {
+	int c;
 
-
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
 }
