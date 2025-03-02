d98233bcf6f06972632dbb5e89fdfe4368beb798
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-02 16:12:20 +0000

    i copy the code of the 7-diagonals and i replace a space by a # in the first loop

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,13 +2,27 @@
 
 /**
  * print_triangle - Function for prints a triangle
- * @?: ???
+ * @size: Variable for prints a triangle
  * Return: 1 (Error)
  */
 
 void print_triangle(int size)
 {
 
+	int horizontal;
+	int vertical;
 
-
+	for (horizontal = 0; horizontal <= size; horizontal++)
+	{
+		for (vertical = 0; vertical <= horizontal; vertical++)
+		{
+			_putchar ('#');
+		}
+		_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar (' ');
+	}
 }
