a085ce3d03d8f881e19e40a694f59ba3b83ee59c
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-02 17:43:45 +0000

    add a four condition and the modulos for multiple of 3, of 5 or multiple of 3 and 5

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,52 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
  * main - Function for realize Fizz Buzz
  * @???: ???
- * Return: ??? (???)
+ * Return: 0 (Sucess)
  */
 
 int main(void)
 {
 
+	int base;
 
+	for (base = 0; base <= 100; base++)
+	{
+		if (base == % 3)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if (base == % 5)
+		{
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if (base == % 3 && base == % 5)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d ", base);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
