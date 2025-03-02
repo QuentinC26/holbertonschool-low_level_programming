3c86e2c06f04ef11b32683ab3bb17ff679e8477b
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-02 09:57:26 +0000

    void print_square(int size);

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -8,7 +8,8 @@ int mul(int a, int b); /**Function for  multiplies two integers*/
 void print_numbers(void); /**Function for prints the number from 0 at 9*/
 void print_most_numbers(void); /**Function for prints the number without 2 and 4*/
 void more_numbers(void); /**Function for prints 10 times number from 0 at 14*/
-void print_line(int n); /** function that draws a straight line*/
+void print_line(int n); /** Function that draws a straight line*/
 void print_diagonal(int n); /** Function that draws a diagonal line*/
+void print_square(int size); /** Function that prints a square*/
 
 #endif /* MAIN_H */
