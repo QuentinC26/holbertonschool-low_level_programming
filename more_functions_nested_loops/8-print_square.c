92b1d9d50464e4ba3c9b27ee28e397af0adf4b4f
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-02 14:21:28 +0000

    add a two for loop and multiple int

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,34 @@
 #include "main.h"
 
 /**
- * print_diagonal - Function that prints a square line on the terminal
+ * print_square - Function that prints a square line on the terminal
  * @size: Variable for that prints a square line
  * Return: 1 (Error)
  */
 
 void print_square(int size)
 {
-        int c;
+	int horizontal;
+	int vertical;
+	int c;
 
-        if (size <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (c = 0; c < size; c++)
-                {
-                        _putchar('#');
-                }
-                _putchar('\n');
-        }
+	for (horizontal = 0; horizontal <= 9; horizontal++)
+	{
+		for (vertical = 0; vertical <= 9; vertical++)
+		{
+			c = horizontal * vertical;
+			if (size <= 0)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				for (c = 0; c < size; c++)
+				{
+					_putchar('#');
+				}
+				_putchar('\n');
+			}
+		}
+	}
 }
