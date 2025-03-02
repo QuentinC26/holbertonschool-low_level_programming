698110e5d6d0478902c617d274e8ba645c127573
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-02 09:18:37 +0000

    remove a unnecessary spaces in the code

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,17 @@
 void print_line(int n)
 {
 	int c;
- 
+
 	if (n <= 0)
-                {
-                        _putchar('\n');
-                }
-                else
-                {
-			for (c = 0; c < n; c++)
-			{
-				_putchar('_');
-			}
+	{
 		_putchar('\n');
-                }
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+	}
 }
