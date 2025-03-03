f2c07644ef6d5d9eb8d20e1724d9f2911111021d
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-03 12:22:26 +0000

    add a base code for this exercise

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include " main.h
+
+/**
+ * _memset - Function for fills the bytes on the memory
+ * @.: ???
+ * Return: 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+
+}
