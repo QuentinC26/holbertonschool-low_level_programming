2b6b937b4cbd0d00cc5050ab95c8e3dbf2a3b514
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-04 14:20:41 +0100

    add char *_strchr(char *s, char c); prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-int _putchar(char c); /* command putchar*/
-void reset_to_98(int *n); /* command for update the value of the pointers*/
+int _putchar(char c); /*command putchar*/
+void reset_to_98(int); /*command for update the value of the pointers*/
 void swap_int(int *a, int *b); /*command for swap the value of two integer number*/
-int _strlen(char *s); /* command for return the length of the string*/
+int _strlen(char *s); /*command for return the length of the string*/
 void _puts(char *str); /* function for that prints a string*/
 void print_rev(char *s); /*function for that prints a reverse string*/
 void rev_string(char *s); /*function for reverse the strings*/
 void puts2(char *str); /*function for prints every other character of string*/
 char *_memset(char *s, char b, unsigned int n); /*function for fills the bytes on the memory*/
 char *_memcpy(char *dest, char *src, unsigned int n); /*Function for copy memory area*/
+char *_strchr(char *s, char c); /*Function for locates a character in a string*/
 
 #endif /* MAIN_H_ */
