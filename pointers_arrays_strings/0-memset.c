0b537430747b451660ad4cea55683f84e31bfd33
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-04 14:07:38 +0100

    completed the foor loop

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,18 +2,20 @@
 
 /**
  * _memset - Function for fills the bytes on the memory
- * @.: ???
+ * @b: Variable for have a constant byte
+ * @s: Variable with pointer
+ * @n: Variable for the array
  * Return: 0 (Success)
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 
-    unsigned int c;
-    
-    for (c = 0; c < n; c++)
-    {
-        s[c] = b;
-    }
-    return (s);
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+	{
+		s[c] = b;
+	}
+	return (s);
 }
