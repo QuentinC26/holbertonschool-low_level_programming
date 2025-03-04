3bdd896edc321dda61e65475b42060a88dd86127
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-04 14:13:14 +0100

    add a comment in the code

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,20 +2,20 @@
 
 /**
  * _memcpy - Function for copy the memory area
- * @?: ???
- * @?: ???
- * @?: ???
+ * @dest: Memory adress if want copy
+ * @scr: Memory source adress
+ * @n: Variable for the Array
  * Return: 0 (Success)
  */
 
 char *_memcpy(char *dest, char *scr, unsigned int n)
 {
 
-    unsigned int c;
-    
-    for (c = 0; c < n; c++)
-    {
-        dest[c] = scr[c];
-    }
-    return (dest);
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+	{
+		dest[c] = scr[c];
+	}
+	return (dest);
 }
