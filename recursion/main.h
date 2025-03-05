47d8f7467c3694ccfd056d1df7ac9a9d171abf3f
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-05 16:49:20 +0100

    add a int factorial(int n); prototype

--- recursion/main.h
+++ recursion/main.h
@@ -5,5 +5,6 @@ int _putchar(char c); /**command putchar*/
 void _puts_recursion(char *s); /**Function for prints a string*/
 void _print_rev_recursion(char *s); /**Function for prints a reverse string */
 int _strlen_recursion(char *s); /**Function for return the lenght of the string */
+int factorial(int n); /** Function for return the factorial of number */
 
 #endif /* MAIN_H_ */
