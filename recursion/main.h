dbe5a90c1a55058685eabfd095ce066514ef076f
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-05 16:07:42 +0100

    add a char *_strchr(char *s, char c); prototype

--- recursion/main.h
+++ recursion/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c); /**command putchar*/
 void _puts_recursion(char *s); /**Function for prints a string*/
 void _print_rev_recursion(char *s); /**Function for prints a reverse string */
+char *_strchr(char *s, char c); /**Function for locates a character string */
 
 #endif /* MAIN_H_ */
