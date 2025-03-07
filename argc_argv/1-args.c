cf7a03442cb74b0bc49c2fb7636e896b294d47dd
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-07 13:02:49 +0100

    add a printf and argv line for error argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Principal function of this code
+ * @argc: Variable integer for count the arguments number
+ * @argv: Array for the pointers of characters who listed all arguments
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
