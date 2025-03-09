6aef2d078ebafcd8540c32488fb3d96ed9e2b8c0
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-09 22:31:56 +0100

    add a base code for this exercise

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - n elements of an array of integers
+* @a: Variable with pointer
+* @n: Element of the array of integers
+* Return: 1 (error):
+*/
+
+void print_array(int *a, int n)
+{
+	
+}
