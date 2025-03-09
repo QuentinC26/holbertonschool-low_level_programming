f2735ba30e5c54cb938adbee40e95ecac5cd22c3
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-09 11:35:33 +0100

    add int _pow_recursion(int x, int y); prototype

--- recursion/main.h
+++ recursion/main.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s); /**Function for prints a string*/
 void _print_rev_recursion(char *s); /**Function for prints a reverse string */
 int _strlen_recursion(char *s); /**Function for return the lenght of the string */
 int factorial(int n); /** Function for return the factorial of number */
+int _pow_recursion(int x, int y); /**Function for that returns the value*/
 
 #endif /* MAIN_H_ */
