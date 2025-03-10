58370ba3ed9cfbde6d2621a74b9672304f6ca89f
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-10 15:44:15 +0100

    add char *_strdup(char *str); prototype

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c); /**command putchar*/
 char *create_array(unsigned int size, char c); /**Function that creates an array of chars */
+char *_strdup(char *str); /**Write a function that returns a pointer*/
 
 #endif /* MAIN_H_ */
