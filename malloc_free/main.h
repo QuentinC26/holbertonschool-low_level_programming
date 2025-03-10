e77d0dfdd7a706c66f4073fe41facb4e609e45bd
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-10 09:56:51 +0100

    add a _putchar and char *create_array(unsigned int size, char c); prototype

---
+++ malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c); /**command putchar*/
+char *create_array(unsigned int size, char c); /**Function that creates an array of chars */
+
+#endif /* MAIN_H_ */
