bc578adc697c414838b51d95742909a38d65f0ca
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-11 21:14:02 +0100

    add void free_grid(int **grid, int height); prototype

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -6,5 +6,6 @@ char *create_array(unsigned int size, char c); /**Function that creates an array
 char *_strdup(char *str); /**Write a function that returns a pointer*/
 char *str_concat(char *s1, char *s2); /**Function that concatenates two strings*/
 int **alloc_grid(int width, int height); /**Function that returns a pointer to a 2 dimensional array of integers. */
+void free_grid(int **grid, int height); /**Function frees a 2 dimensional grid previously created*/
 
 #endif /* MAIN_H_ */
