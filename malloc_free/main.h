deda9ba43748d02cd2cc9652ce1248878afc3fe7
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-11 14:15:46 +0100

    int **alloc_grid(int width, int height);

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -5,5 +5,7 @@ int _putchar(char c); /**command putchar*/
 char *create_array(unsigned int size, char c); /**Function that creates an array of chars */
 char *_strdup(char *str); /**Write a function that returns a pointer*/
 char *str_concat(char *s1, char *s2); /**Function that concatenates two strings*/
+int **alloc_grid(int width, int height); /**Function that returns a pointer 
+to a 2 dimensional array of integers. */
 
 #endif /* MAIN_H_ */
