94e01643e1f87ae6ea280233d3c8769cdd8a1810
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-11 10:31:29 +0100

    add a new prototype

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c); /**command putchar*/
 char *create_array(unsigned int size, char c); /**Function that creates an array of chars */
-char *_strdup(char *str); /**Write a function that returns a pointer*/
+char *strdup(char *str); /**Write a function that returns a pointer*/
 char *str_concat(char *s1, char *s2); /**Function that concatenates two strings*/
 
 #endif /* MAIN_H_ */
