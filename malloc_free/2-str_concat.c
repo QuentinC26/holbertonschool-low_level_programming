b5e5450b3069e92af992d396a67a9a5331914987
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-11 14:45:22 +0100

    replace *s1 and *s2 by s1[] and s2 []

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,14 +19,14 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == 0)
 	{
-		while (*s1 != '\0')
+		while (s1[length] != '\0')
 		{
 		length++;
 		}
 	}
 	if (s2 == 0)
 	{
-		while (*s2 != '\0')
+		while (s2[length_two] != '\0')
 		{
 		length_two++;
 		}
