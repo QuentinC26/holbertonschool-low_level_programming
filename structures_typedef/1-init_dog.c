7b44873dbaaaabe0cf8b8487f8aefeedec2f4b2c
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-12 16:56:53 +0100

    add a if condition and d-> for the new variable

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - Function for initialise a variable of type struct dog
+ * @d: Pointer for give a new variable of the different information
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The Owner of the dog
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
