b70c88078b1d00ab95b12c1e24b437981455f4b2
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-12 15:37:41 +0100

    add struct dog with the typdef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - Base Struct of the code
+ * @name: Pointer to string for determinate the name of the dog
+ * @age: Variable to string for determinate the age of the dog
+ * @owner: Pointer to string for determinate the owner of the dog
+ * Return: 0 Success
+ */
+
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+	typedef struct dog my_dog;
+
+#endif /* DOG_H_ */
+
