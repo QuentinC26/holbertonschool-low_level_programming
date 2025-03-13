18313bd61b2b90e22d2539f0fa09b81f9bb094b7
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-13 13:00:29 +0100

    add a triple condition and triple printf after the condition

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Function for print the struct dog
+ * @d: Pointer for give a new variable of the different information
+ * Return: Always 0.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf(" ");
+	}
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
