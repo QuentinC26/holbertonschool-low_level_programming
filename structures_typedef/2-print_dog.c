3ae8bce352e5bbda001feaaa3a40078327239866
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-13 13:24:46 +0100

    change the order of the code and add two else

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,15 +18,17 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)");
 	}
-	if (!d->age)
+	else
 	{
-		printf("Age: (nil)");
+		printf("Name: %s\n", d->name);
 	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)");
 	}
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
