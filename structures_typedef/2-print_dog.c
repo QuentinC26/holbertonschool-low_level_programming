c0b4038b0838a19c1c87ffc53122c545befe9c24
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-13 13:32:06 +0100

    modify the first condition

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (!d)
 	{
 		return;
 	}
@@ -22,7 +22,9 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
+
 	printf("Age: %f\n", d->age);
+
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)");
