b5b35ad486069e39afcd4f7ddd833bdf813e61e4
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-13 09:24:18 +0100

    retire a comment for the first prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -4,7 +4,7 @@
 /**
  * struct dog - Base Struct of the code
  * @name: Pointer to string for determinate the name of the dog
- * @age: Variable to string for determinate the age of the dog
+ * @age: Variable to float for determinate the age of the dog
  * @owner: Pointer to string for determinate the owner of the dog
  * Return: 0 Success
  */
@@ -16,7 +16,6 @@
 	};
 	typedef struct dog my_dog;
 
-void init_dog(struct dog *d, char *name, float age, char *owner); /** function 
-that initialize a variable of type */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H_ */
