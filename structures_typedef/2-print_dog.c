ca36d11ce8cc4ba1d6f37f781089310ee92c0234
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-13 16:45:04 +0100

    add new lines when if value = NULL is true

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -27,7 +27,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
