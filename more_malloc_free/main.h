fb750451283fd55d097728efb44df9396624c2ca
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-14 15:43:35 +0100

    add a char *string_nconcat(char *s1, char *s2, unsigned int n); prototype

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c); /**Fuction for use _putchar command */
 void *malloc_checked(unsigned int b); /**Function for allocate memory using malloc*/
+char *string_nconcat(char *s1, char *s2, unsigned int n); /**Function for concatenates two strings. */
 
 #endif /* MAIN_H_ */
