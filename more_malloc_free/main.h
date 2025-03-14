3717f6f9068116a2316176e9d10684f703d595bd
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-14 14:37:06 +0100

    add a putchar prototype

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-void *malloc_checked(unsigned int b); /**Function for allocate memory using*/
+int _putchar(char c); /**Fuction for use _putchar command */
+void *malloc_checked(unsigned int b); /**Function for allocate memory using malloc*/
 
 #endif /* MAIN_H_ */
