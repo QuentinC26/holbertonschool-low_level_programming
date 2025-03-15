cd5e9aeccc2b3cff5a96c70ceee5b1b9ad9cb3b7
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-15 22:49:31 +0100

    add a while loop and for loop for copy src in dust

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - n elements of an array of integers
+ * @dest: Second string with pointer
+ * @src: First string with pointer
+ * Return: 1 (error):
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+	int length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	for (count = 0; count < length; count++)
+	{
+		dest[count] = src[count];
+	}
+	return (dest);
+}
