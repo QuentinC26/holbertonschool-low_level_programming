abcb4eee99da7af14356bb8c5da581288b8fd99f
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-15 22:07:21 +0100

    add a for loop with a if condition

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,13 +2,24 @@
 #include <stdio.h>
 
 /**
-* print_array - n elements of an array of integers
-* @a: Variable with pointer
-* @n: Element of the array of integers
-* Return: 1 (error):
-*/
+ * print_array - n elements of an array of integers
+ * @a: Variable with pointer
+ * @n: Element of the array of integers
+ * Return: 1 (error):
+ */
 
 void print_array(int *a, int n)
 {
-	
+	int count;
+
+	for (count = 0; count < n; count++)
+	{
+		printf("%d", a[count]);
+		if (count != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
 }
