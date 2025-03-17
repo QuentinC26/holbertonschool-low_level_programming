23a3252b0f2920fea717f7933007ca5dc92cd1ff
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-17 14:41:04 +0100

    add a double confition and f(name)

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Function that print a name
+ * @name: Pointer of print name
+ * @f: Function pointer
+ * Return: 0 (Success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
