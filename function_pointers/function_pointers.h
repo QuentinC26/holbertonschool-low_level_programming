9bbb2a7176711a76af2f5f5f59e9a2ddb6d6d4fa
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-17 13:50:49 +0100

    add _putchard command and void print_name(char *name, void (*f)(char *)); prototype

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c); /* Function for have _putchar command */
+void print_name(char *name, void (*f)(char *)); /* Function for print a name */
+
+#endif /* MAIN_H_ */
