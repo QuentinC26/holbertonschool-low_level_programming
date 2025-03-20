f5a1e6219d0e590d2f41789b0cc0599960fa1de1
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-21 00:48:35 +0100

    add a if condition

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,6 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
+		if (n = 0)
+		{
+			return;
+		}
 		if (memory == NULL)
 		{
 			printf("nil");
