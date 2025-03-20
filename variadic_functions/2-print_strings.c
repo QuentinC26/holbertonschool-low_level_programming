53799d004ea17de029e1690cf7c6cc91136cd295
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-21 00:04:09 +0100

    add for with two if condition

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_strings - function that prints strings, followed by a new line.
+ *@separator: the string to be printed between strings
+ *@n: the number of strings passed to the function
+ * Return: Always 0.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int list = 0;
+
+	va_start(args, n);
+	for (list = 0; list < n; list++)
+	{
+		if (list > 0 && separator == NULL)
+		{
+			printf("nil");
+		}
+		if (list > 0 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		printf("%s", va_arg(args, char*));
+	}
+	va_end(args);
+	printf("\n");
+}
