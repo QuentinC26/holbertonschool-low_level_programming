c436bfa0ae3a27214045515116c1bee2f57df173
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-20 21:52:12 +0100

    add void print_numbers(const char *separator, const unsigned int n, ...); prototype

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H_
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
-#endif /* MAIN_H_ */
+#endif /* VARIADIC_FUNCTIONS_H_ */
