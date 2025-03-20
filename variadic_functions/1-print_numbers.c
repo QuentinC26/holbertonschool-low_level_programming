ba7d1894284528d345ba83195a300fb8ce820871
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-20 23:26:01 +0100

    add a va multiple command, for loop with if inside the loop and if outsite the for loop

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line.
+ *@separator: the string to be printed between numbers
+ *@n: the number of integers passed to the function
+ * Return: Always 0.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int list = 0;
+
+	va_start(args, n);
+	for (list = 0; list < n; list++)
+	{
+		if (list > 0)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(args, int));
+	}
+	if (separator != NULL)
+	{
+		va_arg(args, int);
+	}
+	va_end(args);
+	printf("\n");
+}
