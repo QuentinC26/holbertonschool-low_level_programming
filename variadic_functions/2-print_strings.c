77308731a638b19f5a9ba88a78c2e70adcea5f7b
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-21 00:51:56 +0100

    remove a if condition with n == 0

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,10 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *memory;
 
 	va_start(args, n);
-	if (n == 0)
-	{
-		return;
-	}
 	for (list = 0; list < n; list++)
 	{
 		memory = va_arg(args, char*);
