28c61202f16ffe4052d195910e006ae4ef3414db
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-21 00:37:04 +0100

    separe the condition list > 0 && separator == NULL of two if condition

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (list = 0; list < n; list++)
 	{
 		memory = va_arg(args, char*);
-		if (list > 0 && separator != NULL)
+		if (separator == NULL)
+		{
+			return;
+		}
+		if (list > 0)
 		{
 			printf("%s", separator);
 		}
