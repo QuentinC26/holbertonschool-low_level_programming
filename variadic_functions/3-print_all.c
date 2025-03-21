ccda17dac129e071a6212c3b11e02a5edca728b7
QuentinC26 <q.zuzlewski@gmail.com>
2025-03-21 16:33:44 +0100

    add a while with switch and case instructions and if condition

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_all - function that prints anything.
+ * @format: list of types of arguments passed to the function
+ * Return: Always 0.
+ */
+
+void print_all(const char *const format, ...)
+{
+	va_list args;
+	int count = 0;
+	char *s = 0;
+
+	va_start(args, format);
+	while (format && format[count])
+	{
+		switch (format[count])
+		{
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				printf("%s", va_arg(args, char *));
+				if (s == NULL)
+				{
+					s = "(nil)";
+				}
+				break;
+			default:
+				count++;
+				continue;
+		}
+		if (format[count + 1] != '\0')
+		{
+			printf(", ");
+		}
+		count++;
+	}
+	printf("\n");
+	va_end(args);
+}
