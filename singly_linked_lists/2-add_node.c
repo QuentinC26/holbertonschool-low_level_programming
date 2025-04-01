8fdc82ce4c7939dd00933d5cad53844d6756d38c
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-01 16:35:45 +0200

    add a if condition for check mode_two = NULL and add mode_two->str, mode_two->len and mode_two->next for add a node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *add_node - function that adds a new node at the beginning of a list_t
+ *@head: the header of the linked list
+ *@str: the string where you want to retrieve its width
+ *struct list_number_two : singly linked list
+ *Return: 0 (Success)
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node_two = malloc(sizeof(list_t));
+
+	if (node_two == NULL)
+	{
+		free(node_two);
+		return (NULL);
+	}
+
+	node_two->str = strdup(str);
+	node_two->len = strlen(str);
+	node_two->next = *head;
+
+	*head = node_two;
+
+	return (node_two);
+}
