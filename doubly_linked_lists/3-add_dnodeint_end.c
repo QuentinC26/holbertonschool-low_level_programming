5f2ece236f0d47ce6867de5ccf7a03dd74e75cde
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-03 16:49:03 +0200

    add a return null in the condition head = null

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -45,6 +45,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (NULL);
 	}
 	else
 	{
@@ -54,6 +55,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		}
 		lastnode->next = new_node;
 	}
-
 	return (new_node);
 }
