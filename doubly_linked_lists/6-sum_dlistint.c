80c8bbecf08369199437c7633330b99becbae7fe
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-04 12:15:59 +0200

    add the addition in the while loop

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *sum_dlistint - function that returns the nth node of a dlistint_t
+ *@head: the header of the node
+ *@index: item to be retrieved from the list
+ *Return: 1 (Error)
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+	int sum = 0;
+
+	node = head;
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (node != NULL)
+	{
+		sum += node->n;
+		node = node->next;
+	}
+	return (sum);
+}
