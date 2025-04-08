1172998bdffc22024ca5e584033a92687427b97d
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-08 09:41:10 +0200

    remove the ssize_t result and im add else condition

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,35 +10,26 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t f_d;
-	int count = 0;
-	ssize_t result;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		return (-1);
-	}
 
-	f_d = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0666);
+	f_d = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
 	if (f_d == -1)
 	{
 		return (-1);
 	}
-
-	while (text_content[count] != '\0')
+	if (text_content == NULL)
 	{
-		count++;
+		f_d = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
+		return (1);
 	}
-
-	result = write(f_d, text_content, count);
-	if (result == -1)
+	else
 	{
-		return (-1);
+		f_d = write(f_d, text_content, strlen(text_content));
 	}
-
 	close(f_d);
 	return (1);
 }
