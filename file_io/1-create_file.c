d77ee2253bdf8c166c9408b9e6152857ca78482f
QuentinC26 <q.zuzlewski@gmail.com>
2025-04-08 10:31:07 +0200

    modify the order in the two open system call

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,14 +16,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	f_d = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
+	f_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (f_d == -1)
 	{
 		return (-1);
 	}
 	if (text_content == NULL)
 	{
-		f_d = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
+		f_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		return (1);
 	}
 	else
